d74ecc48e6e40a8ea9096a5bafbed656c80af365
sagarmitukula5708-maker <sagarmitukula5708@gmail.com>
2025-09-17 17:51:26 +0530

    app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if uploaded_file is not None and api_key:
     df = pd.read_csv(uploaded_file)
     st.write("Uploaded Keywords:", df.head())
     
-    openai.api_key = api_key
+    gemini.api_key = AIzaSyBktncfXblCucdOqXI_PEM9WK2CQsktxUo
     
     # Function to analyze a single keyword
     def analyze_keyword(keyword):
